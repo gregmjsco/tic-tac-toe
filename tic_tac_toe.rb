937364625f5b0d40b4c12ca3995cd998523e1be8
Greg Johnston <gregmj.sco@gmail.com>
2022-09-29 23:36:44 +0900

    Restructure classes and start to add to Player class

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -7,20 +7,29 @@ $LINES = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
 
 class Player
 
-    attr_accessor :player
-    def initialize
-        puts "Player created"
+    attr_accessor :name, :marker
+    def initialize(name, marker)
+        @name = name 
+        @marker = marker
+        puts "#{self.name} is player using #{self.marker}"
+    end
+
+    
+
+    def user_input
+        gets.to_i
     end
 end
 
-class Board 
+class Game  
     attr_accessor :board
+    turn_count = 1
 
     def initialize
         @board = Array.new(9) {|i| i.to_s }
     end
 
-    def show_board
+    def show_board()
             puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
             puts "-----------"
             puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
@@ -29,7 +38,7 @@ class Board
     end
 end
 
-board = Board.new 
-player1 = Player.new 
+game = Game.new 
+player1 = Player.new('Sam', 'X') 
 
-board.show_board
+game.show_board()
